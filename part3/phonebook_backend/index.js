03bd9ae940d67c55a0db28df481948868d341fef
aniemela-cloud <aniemela@gmail.com>
2026-05-29 16:42:51 +0300

    Part 3 phonebook backend step 7 (3.7) added morgan with stdout logging

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,5 +1,7 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
 
 let persons = [
     {
@@ -25,6 +27,8 @@ let persons = [
 ];
 
 app.use(express.json());
+app.use(morgan('tiny'));
+
 
 app.get('/', (request, response) => {
     response.send('<h1>This is the phonebook backend</h1>')
