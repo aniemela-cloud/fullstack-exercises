aead3e2911e31b2cc881e39ca7fb1be0d0eed950
aniemela-cloud <aniemela@gmail.com>
2026-06-04 16:01:56 +0300

    Part 3 phonebook database step 3 (3.15)

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -58,30 +58,36 @@ app.get('/info', (request, response) => {
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(result => {
         response.json(result);
+    }).catch(error => {
+        console.log('/api/persons error', error);
+        response.status(500).end();
     });
     //response.json(persons)
 });
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
         if (person) {
             response.json(person);
         } else {
             response.status(404).end();
         }        
-    })
+    }).catch(error => {
+        next(error);
+    });
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
-// remove reference to 'persons' object as part of MongoDBifying
-    const idx = -1 //persons.findIndex((person) => person.id === id);
-    if (idx === -1) {
-        response.status(404).end();
-    } else {
-        persons.splice(idx,1); // delete the found element by index
-        response.status(204).end();
-    }
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id).then(result => {
+        if(result) {
+            response.status(204).end();
+        }
+        else {
+            response.status(404).end();
+        }
+    }).catch(error => {
+        next(error);
+    });
 })
 
 app.post('/api/persons', (request, response) => {
@@ -124,11 +130,20 @@ app.post('/api/persons', (request, response) => {
         response.json(result);
     }).catch(error => {
         console.log('Error saving new phonebook entry: ',error);
-        response.status(500).end()
+        response.status(500).end();
     })    
     
 })
 
+const mongoCastErrorHandler = (error, request, response, next) => {
+    console.error(error.message);
+    if (error.name == "CastError") {
+        response.status(400).send({ error: 'malformatted id' });        
+    }
+    next(error);
+}
+
+app.use(mongoCastErrorHandler);
 
 const PORT = process.env.port || 3001
 app.listen(PORT, () => {
