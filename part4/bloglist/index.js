dd405e44cb6c5b1e44a991d3cffd83c66e39206d
aniemela-cloud <aniemela@gmail.com>
2026-06-05 14:09:07 +0300

    refactoring: DB model and URI path controllers separated into modules

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,37 +1,16 @@
 const config = require('./utils/config')
 const express = require('express')
+const blogRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 
 const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { family: 4 })
 
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
+app.use('/api/blogs', blogRouter)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
