17bb314086dfa86b46ba642beaad92f2562d14c8
aniemela-cloud <aniemela@gmail.com>
2026-06-05 14:44:30 +0300

    Part 4 Helper Functions and Unit Tests, step 2 (4.4) total likes

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+    if(!blogs || blogs.length === 0) {
+        return 0
+    }
+    return blogs.reduce((sum, item) => sum + item.likes, 0)
+}
+
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
