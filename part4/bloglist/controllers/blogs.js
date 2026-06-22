a29293576890f937b73665b3f5eaa930aea9670f
aniemela-cloud <aniemela@gmail.com>
2026-06-22 16:20:13 +0300

    4.10: blog List Tests, step 3 -- added POST tests, and also modified controller to reject malformed POST requests. One #TODO is filtering out any extra fields in the JSON data sent from the browser, now it actually just saves everything, not just the author/title/url...

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -12,7 +12,33 @@ blogRouter.get('/:id', async (request, response) => {
 })
 
 blogRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+  const blogpost_data = request.body
+  if (!blogpost_data) {
+    return response.status(400).json({
+      error: 'empty request body'
+    })
+  }
+  if (typeof (blogpost_data) !== 'object') {
+    return response.status(400).json({
+      error: 'malformed request body'
+    })
+  }
+  if (!blogpost_data.author) {
+    return response.status(400).json({
+      error: 'author field missing'
+    })
+  }
+  if (!blogpost_data.title) {
+    return response.status(400).json({
+      error: 'title field missing'
+    })
+  }
+  if (!blogpost_data.url) {
+    return response.status(400).json({
+      error: 'url field missing'
+    })
+  }
+  const blog = new Blog(blogpost_data)
 
   blog.save().then((result) => {
     response.status(201).json(result)
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -59,11 +59,11 @@ const blogs_testdata = [
 
 beforeEach(async () => {
     await Blog.deleteMany({})
-    console.log('cleared database')
+    //console.log('cleared database')
     const blogMongoObjList = blogs_testdata.map(blog => new Blog(blog))
     const promiseArray = blogMongoObjList.map(blogObj => blogObj.save())
     await Promise.all(promiseArray)
-    console.log('data re-insterted')
+    //console.log('data re-insterted')
 })
 describe('api/blogs GET endpoint', () => {
     test('blog posts are returned as JSON', async () => {
@@ -81,9 +81,109 @@ describe('api/blogs GET endpoint', () => {
         const response = await api.get('/api/blogs')
         assert.ok(response.body[0].id, "First result does not have a non-false 'id' field")
         const byIdResponse = await api.get(`/api/blogs/${response.body[0].id}`)
-        assert.strictEqual(response.body[0].id, byIdResponse.body.id, 
+        assert.strictEqual(byIdResponse.body.id, response.body[0].id, 
             "Searching by 'id' of the first found result did not give result with same 'id'")
     })
+})
+describe('api/blogs POST endpoint', () => {
+    test('POST without author fails with status 400', async () => {
+        const post_no_author = {
+            title: "Authorless Post",
+            url: "http://example.url.com/foo/bar",
+        }
+        await api
+            .post('/api/blogs')
+            .send(post_no_author)
+            .expect(400)
+    })
+
+    test('POST without title fails with status 400', async () => {
+        const post_no_title = {
+            author: "Author Notitle",
+            url: "http://example.url.com/foo/bar",
+        }
+        await api
+            .post('/api/blogs')
+            .send(post_no_title)
+            .expect(400)
+    })
+
+    test('POST without URL fails with status 400', async () => {
+        const post_no_url = {
+            title: "Post With No Url",
+            author: "Example Author",
+        }  
+        await api
+            .post('/api/blogs')
+            .send(post_no_url)
+            .expect(400)
+    })
+
+    test('POST with appropriate fields included returns 201', async () => {
+        const post_good = {
+            title: "Post With All Fields",
+            author: "Example Author",
+            url: "http://this.is.an.url.com/foo/bar"
+        }  
+        await api
+            .post('/api/blogs')
+            .send(post_good)
+            .expect(201)
+        
+    })
+    test('succesful POST returns JSON', async () => {
+        const post_good = {
+            title: "Post With All Fields",
+            author: "Example Author",
+            url: "http://this.is.an.url.com/foo/bar"
+        }  
+        await api
+            .post('/api/blogs')
+            .send(post_good)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+    })
+    test('succesful POST increases number of GET results by one', async () => {
+        const post_good = {
+            title: "Post With All Fields",
+            author: "Example Author",
+            url: "http://this.is.an.url.com/foo/bar"
+        }  
+        const pre_results = await api.get('/api/blogs')
+        await api
+            .post('/api/blogs')
+            .send(post_good)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+        const after_results = await api.get('/api/blogs')
+        assert.strictEqual(after_results.body.length, pre_results.body.length+1)
+    })
+    test('JSON result from succesful POST matches sent data', async () => {
+        const post_good = {
+            title: "Post With All Fields",
+            author: "Example Author",
+            url: "http://this.is.an.url.com/foo/bar"
+        }  
+        const post_result = await api
+            .post('/api/blogs')
+            .send(post_good)
+        assert.partialDeepStrictEqual(post_result.body, post_good)
+    })
+    test('A result can be found using id read from JSON result of succesful POST', async () => {
+        const post_good = {
+            title: "Post With All Fields",
+            author: "Example Author",
+            url: "http://this.is.an.url.com/foo/bar"
+        }  
+        const post_result = await api
+            .post('/api/blogs')
+            .send(post_good)
+        
+        const get_result = await api.get(`/api/blogs/${post_result.body.id}`)
+        assert.partialDeepStrictEqual(post_result.body, get_result.body)
+    })
+
+
 })
 after(async () => {
   await mongoose.connection.close()
