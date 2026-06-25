972b59113e3a35f7334ea4685b8ac3db5d2d6162
aniemela-cloud <aniemela@gmail.com>
2026-06-25 18:48:23 +0300

    5.1: Blog List Frontend, step 1; login and conditional blog list rendering

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -6,10 +6,22 @@ const loginRouter = require('./controllers/login')
 const middleWare = require('./utils/middleware')
 const mongoose = require('mongoose')
 const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { family: 4 })
+
+app.use(cors())
 app.use(express.json())
+if (process.env.NODE_ENV !== "test") {
+    morgan.token('rq-body', (req) => {
+        return JSON.stringify(req.body)
+    })
+    app.use(morgan('tiny', { skip: (req) => { return req.method === 'POST' } }))
+    app.use(morgan(':method :url :status :res[content-length] - :response-time ms :rq-body',
+        { skip: (req) => { return req.method !== 'POST' } }))
+}
 app.use(middleWare.tokenExtractor)
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +15,61 @@ const App = () => {
     )  
   }, [])
 
-  return (
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({username, password})
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch(error) {
+      console.error('caught error ',error)
+    }
+    console.log('handleLogin called username:',username, 'password', password)
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        <h2>login</h2>
+      </div>
+      <div>
+        <label>
+          username
+          <input
+            type="text"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          password
+          <input
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <button type="submit">login</button>
+      </div>
+    </form>
+  )
+  const blogList = () => (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+        {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
-      )}
+        )}
+    </div>
+  )
+  return (
+    <div>
+      {(!user && loginForm())}
+      {(user && blogList())}
     </div>
   )
 }
