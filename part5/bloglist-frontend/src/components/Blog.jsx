8596f29b35eec7e3edc9b0c651fa2af7f8f97316
aniemela-cloud <aniemela@gmail.com>
2026-06-26 14:04:37 +0300

    5.7 Blog List Frontend, step 7; blog entry detail show/hide button and functionality

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
 
+  //const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} by {blog.author} 
+      <button 
+        name="show toggle"
+        onClick={toggleVisibility}>
+           { visible ? 'hide' : 'show' } 
+      </button>
+      <div style={showWhenVisible}>
+        <div>URL: {blog.url}</div>
+        <div>Likes: {blog.likes} <button name="like">like</button></div>
+        <div><em>({blog.user.name})</em></div>
+      </div>
+    </div>
+  )
+}
 export default Blog
