b4fc2efb622c069c13155543e063262207a3c4c2
aniemela-cloud <aniemela@gmail.com>
2026-06-29 19:29:56 +0300

    Increase playwright total timeout to handle slow server responses

--- part5/bloglist-playwright/playwright.config.js
+++ part5/bloglist-playwright/playwright.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   workers: 1,
-  timeout: 3000,
+  timeout: 10000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
