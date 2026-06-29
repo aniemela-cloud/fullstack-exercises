0c57626c03f8a53c94925543e2699e453ae67f6f
aniemela-cloud <aniemela@gmail.com>
2026-06-29 15:55:05 +0300

    5.19: Blog List End To End Testing, step 3

--- part5/bloglist-playwright/tests/bloglist.spec.js
+++ part5/bloglist-playwright/tests/bloglist.spec.js
@@ -1,5 +1,6 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 const helper = require('./helper')
+const { before } = require('node:test')
 
 describe('Blog app', () => {
     beforeEach(async ({ page, request }) => {
@@ -44,6 +45,53 @@ describe('Blog app', () => {
             await expect(page.getByText(`${helper.test_name} logged in`,
                 { exact: false })).toBeVisible()
         })
+    })
+
+    describe('Operations with logged in user', () => {
+        beforeEach(async ({ page }) => {
+            await helper.loginWith(page, helper.test_username, helper.test_password)
+        })
+        test('Clicking on \'Add a blog\' shows \'add blog\' form', async ({ page }) => {
+            const unfold_button = page.getByRole('button', { name: 'Add a blog' })
+            await unfold_button.click()
+
+            await expect(page.getByLabel(/author/i, { exact: false })).toBeVisible()
+            await expect(page.getByLabel(/blog title/i, { exact: false })).toBeVisible()
+            await expect(page.getByLabel(/url/i, { exact: false })).toBeVisible()
+            await expect(page.getByRole('button', { name: /add blog/i})).toBeVisible()
+        })
+
+        test('Clicking on \'cancel\' when \'add blog\' form is visible hides the form', async ({ page }) => {
+            const unfold_button = page.getByRole('button', { name: 'Add a blog' })
+            await unfold_button.click()
+
+            const cancel_button = page.getByRole('button', { name: /cancel/i })
+            await cancel_button.click()
+
+            await expect(page.getByLabel(/author/i, { exact: false })).not.toBeVisible()
+            await expect(page.getByLabel(/blog title/i, { exact: false })).not.toBeVisible()
+            await expect(page.getByLabel(/url/i, { exact: false })).not.toBeVisible()
+            await expect(page.getByRole('button', { name: /add blog/i})).not.toBeVisible()
+        })
+
+        test('A new blog is added when the \'add blog\' form is submitted', async ({ page }) => {
+            const unfold_button = page.getByRole('button', { name: 'Add a blog' })
+            await unfold_button.click()
+
+            const test_author = 'Test Author'
+            const test_title = 'Test Blog Entry'
+            const test_url = 'http://test.url.is'
+
+            await page.getByLabel(/author/i, { exact: false}).fill(test_author)
+            await page.getByLabel(/blog title/i, { exact: false }).fill(test_title)
+            await page.getByLabel(/url/i, { exact: false }).fill(test_url)
+            await page.getByRole('button', { name: /add blog/i}).click()
+
+            await expect(page.getByText(`${test_title} by ${test_author} added.`, { exact:false })).toBeVisible()
+            // div.blog span.blog_title
+            await expect(page.locator('div.blog > span.blog_title', { hasText: `${test_title}`})).toBeVisible()
+        })
+
     })
 
 })
