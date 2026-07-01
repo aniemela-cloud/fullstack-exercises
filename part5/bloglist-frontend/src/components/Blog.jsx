db2aa2a85b1cf72f69e8d71e7d4dbabb04c2099b
aniemela-cloud <aniemela@gmail.com>
2026-07-01 20:14:25 +0300

    5.31: styled blogs, step 3

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+
+import { Box, Typography, Stack, Link, Button, Divider } from '@mui/material'
 const Blog = ({ user, blog, updateLike, deleteBlog }) => {
   //const [visible, setVisible] = useState(false)
   const [likes, setLikes] = useState(blog ? blog.likes : 0)
@@ -8,7 +10,7 @@ const Blog = ({ user, blog, updateLike, deleteBlog }) => {
   const navigate = useNavigate()
 
   useEffect(() => {
-    if(!blog) {
+    if (!blog) {
       navigate('/')
     }
   }, [blog, navigate])
@@ -36,19 +38,22 @@ const Blog = ({ user, blog, updateLike, deleteBlog }) => {
   }
   if (blog) {
     return (
-      <div className="blog" style={blogStyle}>
-        <h3 className="blog_title">{blog.title}</h3>
-        <p className="blog_author">{blog.author}</p>
-        <div className="blog_url">URL: <a href={blog.url}>{blog.url}</a></div>
-        <div className="blog_likes">Likes: {likes}
-          {user && (<button name="like" onClick={onLike}>like</button>)}</div>
-        <div className="blog_user"><em>({blog.user ? blog.user.name : 'missing information'})</em></div>
-        {user && blog.user && user.username === blog.user.username && (
-          <div className="blog_delete">
-            <button name="delete" onClick={onDelete}>DELETE</button>
-          </div>
-        )}
-      </div>
+      <Box className="blog" sx={{ m: 2 }}>
+        <Typography variant="h3" className="blog_title">{blog.title}</Typography>
+        <Typography variant="subtitle1" className="blog_author">{blog.author}</Typography>
+        <Divider/>
+        <Stack spacing={1} sx={{ marginTop: 3 }}>
+          <Link className="blog_url" href={blog.url}>{blog.url}</Link>
+          <div className="blog_likes">Likes: {likes}</div>
+          <div className="blog_user"><em>Added by {blog.user ? blog.user.name : 'missing information'}</em></div>
+          <Stack direction="row">
+            {user && (<Button  variant="outlined" name="like" color="secondary" onClick={onLike}>like</Button>)}
+            {user && blog.user && user.username === blog.user.username && (
+              <Button name="delete" variant="outlined" color="error" onClick={onDelete}>DELETE</Button>
+            )}
+          </Stack>
+        </Stack>
+      </Box>
     )
   }
 }
