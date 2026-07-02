cdfbef2ca0f62f95426612984d82279fdd8099a2
aniemela-cloud <aniemela@gmail.com>
2026-07-02 19:04:29 +0300

    6.7 anecdotes, step6

--- part6/zustand-anecdotes/src/App.jsx
+++ part6/zustand-anecdotes/src/App.jsx
@@ -1,8 +1,21 @@
 import Filter from './components/Filter'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
+import { useEffect } from 'react'
+import { useAnecdoteActions } from './store'
+import anecdoteService from './services/anecdotes'
 
 const App = () => {
+  const { initialize } = useAnecdoteActions()
+  
+  useEffect(() => {
+    async function fetchData() {
+      const fetchedAnecdotes = await anecdoteService.getAll()
+      initialize(fetchedAnecdotes)
+    }
+    fetchData()
+  }, [initialize])
+  
   return (
     <div>
       <h2>Anecdotes</h2>
