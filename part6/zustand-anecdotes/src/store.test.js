ee270362d2870644f127b26d67ca4918195a8389
aniemela-cloud <aniemela@gmail.com>
2026-07-03 16:19:52 +0300

    6.14 Anecdotes, step13

--- part6/zustand-anecdotes/src/store.test.js
+++ part6/zustand-anecdotes/src/store.test.js
@@ -12,7 +12,7 @@ vi.mock('./services/anecdotes', () => ({
 }))
 
 import anecdoteService from './services/anecdotes'
-import useAnecdoteStore, { useAnecdotes, useFilter, useAnecdoteActions } from './store'
+import useAnecdoteStore, { useAnecdotes, useAnecdoteActions } from './store'
 
 beforeEach(() => {
   useAnecdoteStore.setState({ anecdotes: [], filter: '' })
@@ -35,6 +35,7 @@ describe('Anecdote Store actions', () => {
     const { result: anecdotesResult } = renderHook(() => useAnecdotes())
     expect(anecdotesResult.current).toEqual(mock_anecdotes)
   })
+  
   test('useAnecdotes() returns anecdotes sorted by votes', async () => {
     const mock_anecdotes = [
       { id: '1', content: 'Test', votes: 0 },
@@ -54,5 +55,43 @@ describe('Anecdote Store actions', () => {
     const { result: anecdotesResult } = renderHook(() => useAnecdotes())
     expect(anecdotesResult.current).toEqual(sorted_anecdotes)
 
+  })
+
+  test('useAnecdote() filters anecdotes based on the set filter', async () => {
+    const aaa_anecdotes = [
+      { id: '1', content: 'Aaa Test', votes: 5 },
+      { id: '2', content: 'Aaa Test 2', votes: 4 },
+      { id: '3', content: 'Aaa Test 3', votes: 3 },      
+    ]
+    const bbb_anecdotes = [
+      { id: '4', content: 'Bbb Test', votes: 2 },
+      { id: '5', content: 'Bbb Test 2', votes: 1 },
+      { id: '6', content: 'Bbb Test 3', votes: 0 },      
+    ]
+    const xxx_anecdotes = [
+      { id: '4', content: 'Xxx Test', votes: 2 },
+      { id: '5', content: 'Xxx Test 2', votes: 1 },
+      { id: '6', content: 'Xxx Test 3', votes: 0 },      
+    ]
+    const mock_anecdotes = aaa_anecdotes.concat(bbb_anecdotes, xxx_anecdotes)
+
+    anecdoteService.getAll.mockResolvedValue(mock_anecdotes)
+
+    const { result } = renderHook(() => useAnecdoteActions())
+
+    await act(async () => {
+      await result.current.initialize()
+      await result.current.setFilter('aaa')
+    })
+
+    const { result: aaa_anecdotesResult } = renderHook(() => useAnecdotes())
+    expect(aaa_anecdotesResult.current).toEqual(aaa_anecdotes)
+
+    await act(async () => {
+      await result.current.setFilter('bbb')
+    })
+
+    const { result: bbb_anecdotesResult } = renderHook(() => useAnecdotes())
+    expect(bbb_anecdotesResult.current).toEqual(bbb_anecdotes)
   })
 })
